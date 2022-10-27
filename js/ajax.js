51069e9f5dc4f958870a96d0026a4ce819a94b29
ianchan3 <ianchan3138@gmail.com>
2022-10-27 16:18:09 -0700

    JavaScript Completed

--- js/ajax.js
+++ js/ajax.js
@@ -1,4 +1,4 @@
-const btn = document.querySelector("get-quotes");
+const btn = document.querySelector(".get-quotes");
 
 btn.addEventListener("click", getQuotes);
 
@@ -9,7 +9,19 @@ function getQuotes(e) {
 
   https.open("GET", "https://type.fit/api/quotes", true)
   https.onload = function() {
+    if (this.status === 200) {
 
+      const response = JSON.parse(this.responseText);
+      let output = ""
+      response.forEach(function(quote) {
+        output += `
+            <li>Quote: ${quote.text}</li>
+            <li>Author: ${quote.author}</li>
+            <hr>
+        `
+      })
+      document.querySelector(".quotes").innerHTML = output;
+    }
   }
 
   https.send();
